af093f948e412334d2c661e97aadc22e46dc9893
Julian Schäfer <schaefer@synyx.de>
2020-11-13 13:35:06 +0100

    Add styling for coffee cup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   theme: {
+    screens: {
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
     extend: {
       colors: {
         coffeenet: {
@@ -7,6 +12,13 @@ module.exports = {
           light: "#9E9E9E",
           dark: "#404042",
         },
+      },
+      maxWidth: {
+        10: "10vw",
+        20: "20vw",
+        25: "25vw",
+        35: "35vw",
+        80: "80vw",
       },
     },
   },
